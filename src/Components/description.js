cc9a593762caede8e1e50e3ff0f267de2c2cd405
Rohit Kumar <75325602+Rohit-Alex@users.noreply.github.com>
2021-04-11 20:56:40 +0530

    Update description.js

--- src/Components/description.js
+++ src/Components/description.js
@@ -2,41 +2,56 @@ import React from "react";
 import "./Description.css";
 import { Link } from "react-router-dom";
 
-function description({ questionOne, questionTwo, allOption1, allOption2 }) {
+function description({
+  questionOne,
+  questionTwo,
+  allOption1,
+  allOption2,
+  renderQ1,
+  renderQ2
+}) {
   return (
     <div className="container_des">
       <h1>Question Description</h1>
-      <div className="subone">
-        <h4>{questionOne}</h4>
+      {renderQ1 && (
+        <>
+          <div className="subone">
+            <h4>{questionOne}</h4>
 
-        {allOption1.map((item, index) => {
-          return (
-            <>
-              <div className="item">
-                <input type="checkbox" />
-                {item}
-              </div>
-              <br />
-            </>
-          );
-        })}
-      </div>
+            {allOption1.map((item, index) => {
+              return (
+                <>
+                  <div className="item">
+                    <input type="checkbox" />
+                    {item}
+                  </div>
+                  <br />
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
 
-      <div className="subone">
-        <h4>{questionTwo}</h4>
-        {allOption2.map((item) => {
-          return (
-            <>
-              <div className="item">
-                <input type="radio" name="radio" />
-                {item}
-              </div>
+      {renderQ2 && (
+        <>
+          <div className="subone">
+            <h4>{questionTwo}</h4>
+            {allOption2.map((item) => {
+              return (
+                <>
+                  <div className="item">
+                    <input type="radio" name="radio" />
+                    {item}
+                  </div>
 
-              <br />
-            </>
-          );
-        })}
-      </div>
+                  <br />
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
       <div>
         <Link to="/">
           <button className="btn">Confirm</button>
